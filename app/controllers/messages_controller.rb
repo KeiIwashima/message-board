b4c9008c3a0dcc984a2c05edba4076c87cb79f8f
keiiwashima <uniks.empire@gmail.com>
2016-01-11 11:00:24 +0000

    deletefunction

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path , notice: 'メッセージを削除しました'
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      #保存に失敗した場合はedit画面に戻す
+    render 'edit'
+    end
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
   def index
     @message = Message.new #メッセージモデルの新規インスタンスを作成
     @messages = Message.all #メッセージモデルの全てのインスタンスを取得
