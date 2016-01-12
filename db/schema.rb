7926da03b9c21c6dd361264f9ab8991c8982fab0
keiiwashima <uniks.empire@gmail.com>
2016-01-12 11:52:58 +0000

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111120622) do
+ActiveRecord::Schema.define(version: 20160112115138) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
